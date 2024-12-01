78419780de805e30166f6f6cd57988f8a95e2198
Shimadakunn <lcombaret@hotmail.co.jp>
2024-12-01 13:33:15 +0100

    Refactor font imports and fix HTML syntax in index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import React,{ useEffect } from 'react'
-import Header from './Components/Header.jsx'
-import Mouse from './Components/Mouse.jsx'
-import Home from './Pages/Home.jsx'
-import Gallery from './Pages/Gallery.jsx'
-import Contact from './Pages/Contact.jsx'
-import Anjeli from './Pages/Shoots/Anjeli.jsx'
-import DouEva from './Pages/Shoots/DouEva.jsx'
-import DouWhite from './Pages/Shoots/DouWhite.jsx'
-import Egypte from './Pages/Shoots/Egypte.jsx'
-import EvaBlack from './Pages/Shoots/EvaBlack.jsx'
-import EvaClem from './Pages/Shoots/EvaClem.jsx'
-import EvaClem2 from './Pages/Shoots/EvaClem2.jsx'
-import EvaCyborg from './Pages/Shoots/EvaCyborg.jsx'
-import EvaWhite from './Pages/Shoots/EvaWhite.jsx'
-import EvaWitch from './Pages/Shoots/EvaWitch.jsx'
-import Pomme from './Pages/Shoots/Pomme.jsx'
-import Sarah from './Pages/Shoots/Sarah.jsx'
-import Synda from './Pages/Shoots/Synda.jsx'
-import Moops from './Pages/Shoots/Moops.jsx'
-import Ethan from './Pages/Shoots/Ethan.jsx'
-import Imbh from './Pages/Shoots/Imbh.jsx'
-import Retro from './Pages/Shoots/Retro.jsx'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import Header from "./Components/Header.jsx";
+import Mouse from "./Components/Mouse.jsx";
+import Home from "./Pages/Home.jsx";
+import Gallery from "./Pages/Gallery.jsx";
+import Contact from "./Pages/Contact.jsx";
+import Anjeli from "./Pages/Shoots/Anjeli.jsx";
+import DouEva from "./Pages/Shoots/DouEva.jsx";
+import DouWhite from "./Pages/Shoots/DouWhite.jsx";
+import Egypte from "./Pages/Shoots/Egypte.jsx";
+import EvaBlack from "./Pages/Shoots/EvaBlack.jsx";
+import EvaClem from "./Pages/Shoots/EvaClem.jsx";
+import EvaClem2 from "./Pages/Shoots/EvaClem2.jsx";
+import EvaCyborg from "./Pages/Shoots/EvaCyborg.jsx";
+import EvaWhite from "./Pages/Shoots/EvaWhite.jsx";
+import EvaWitch from "./Pages/Shoots/EvaWitch.jsx";
+import Pomme from "./Pages/Shoots/Pomme.jsx";
+import Sarah from "./Pages/Shoots/Sarah.jsx";
+import Synda from "./Pages/Shoots/Synda.jsx";
+import Moops from "./Pages/Shoots/Moops.jsx";
+import Ethan from "./Pages/Shoots/Ethan.jsx";
+import Imbh from "./Pages/Shoots/Imbh.jsx";
+import Retro from "./Pages/Shoots/Retro.jsx";
 
 function App() {
-  
   return (
     <Router>
       <Header />
@@ -52,7 +51,7 @@ function App() {
         <Route path="/retro" element={<Retro />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
